5a5d8d44dc6d0503a70a963e5cacc5edfb7e40ce
Vichkarichka <Vichkarichka@gmail.com>
2018-03-18 20:34:03 +0300

    Add login in and Sign Up

--- index.js
+++ index.js
@@ -1,192 +1,345 @@
-var valueName,valueSurname,valueAge;
+var valueName, valueSurname, valueAge, valuePassword, valueRole, valueConfirmPasssword;
 var urlPage = "http://localhost:8082/user/";
+var headerId, headerRole;
 
-$(document).ready(function() { 
-	$('#buttonNew').click( function(event){ 
-		modelWindow()
-	});
+$(document).ready(function() {
 
-	
-	$('#modal_close, #overlay, #ADD, #Cancel,#UPDATE').click( function(){ 
-		$('#modal_form, #modal_formTwo, #modal_formThree')
-			.animate({opacity: 0, top: '45%'}, 200,  
-				function(){ 
-					$(this).css('display', 'none'); 
-					$('#overlay').fadeOut(400);
-				}
-			);
-	});
+    $('#buttonNew').hide();
+    $('#buttonShow').hide();
+    $("#Exit").hide();
 
-
-	$("#ADD").click(function(event){
-
-		postPersonsToServer();
-
-	});
-	$('#buttonShow').click(function(event){
-		getPersonsByServer();
-
-	});
-
-
-	function postPersonsToServer(){
-
-		valueName = $("#inputName").val();
-		valueSurname = $("#inputSurname").val();
-		valueAge = $("#inputAge").val();
-		$.ajax({
-			url:"http://localhost:8082/user",
-			type:"POST",
-			data:{name:valueName,surname:valueSurname,age:valueAge },
-			success: function(){
-
-				alert("Add person success");
-				console.log('Successfully connected to the server');
-
-			},
-			error: function(){
-
-				alert("Wrong name or surname or age");
-				console.log('Something went wrong');
-			}
-		});
-	}
-
-	function getPersonsByServer(){
-
-	$.get( "http://localhost:8082/users", function( data ) {
-
-		var tableForPersons = document.createElement('div');
-    if (document.getElementById('userstable')) {
-        var elem = document.getElementById('userstable');
-       elem.parentNode.removeChild(elem);
-    }
-    tableForPersons.setAttribute('id', 'userstable');
-    var table = document.createElement('table');
-    data.forEach(element => {
-        if (element !== null) {
-            var tr = document.createElement('tr');
-            tr.innerHTML ='<td>' + element.name + '</td>' +
-            '<button class="EditByPerson">Edit</button>' + 
-            '<button class="info">Info</button>' + 
-            '<button class="del">Delete</button>';
-            tr.setAttribute('id', element.id);
-            table.appendChild(tr);
-        }
+    $('#modal_close, #overlay, #ADD, #Cancel,#UPDATE, #buttonLoginIn').click(function() {
+        $('#modal_formTwo, #modal_formThree, #modal_formFour, #modal_formFive')
+            .animate({
+                    opacity: 0,
+                    top: '45%'
+                }, 200,
+                function() {
+                    $(this).css('display', 'none');
+                    $('#overlay').fadeOut(400);
+                }
+            );
     });
-    tableForPersons.appendChild(table);
-    document.body.appendChild(tableForPersons);
 
-    	tableForPersons.onclick = function(event) {
+    $("#loginIn").click(function(event) {
 
-        var target = event.target;
-        var targetId = +target.parentNode.getAttribute('id');
+        event.preventDefault();
+        $('#overlay').fadeIn(400,
+            function() {
+                $('#modal_formFour')
+                    .css('display', 'block')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
 
-        if(target.className == 'del') {
-            
-            deletePersonByServer(targetId);
-        }
-        if(target.className == 'info')
-        {
-        	getInfoPersonbyServer(targetId);
-        }
-        if(target.className == 'EditByPerson')
-        {
-        	postUpdatePersonToServer(targetId);
+    });
+    $("#signUp").click(function(event) {
+
+        modelWindow();
+
+    });
+    $("#buttonLoginIn").click(function(event) {
+        $("#loginIn").hide();
+        $("#signUp").hide();
+        $("#Exit").show();
+        postLoginPersonToServer();
+
+    });
+
+    $("#buttonSignUp").click(function(event) {
+
+        postPersonsToServer();
+
+    });
+    $("#Exit").click(function(event) {
+        $('#loginIn').show();
+        $("#signUp").show();
+        $("#Exit").hide();
+        $("#buttonNew").hide();
+        $("#buttonShow").hide();
+        $("table").hide();
+
+    });
+
+    function postLoginPersonToServer() {
+
+        var valueLoginName = $('#loginUserName').val();
+        var valueLoginPassword = $('#passwordLogiIn').val();
+
+        $.ajax({
+            url: "http://localhost:8082/loginuser",
+            type: "POST",
+            data: {
+                name: valueLoginName,
+                password: valueLoginPassword
+            },
+            success: function(result) {
+
+                console.log(result);
+                headerId = result.id;
+                headerRole = result.role;
+                $("#buttonNew").show();
+                $("#buttonShow").show();
+                showButtonForPerson();
+
+            },
+            error: function(result) {
+
+                alert("Wrong name or surname or age");
+                console.log('Something went wrong');
+            }
+        });
+    }
+
+
+    function showButtonForPerson() {
+
+        $('#buttonNew').click(function(event) {
+            modelWindow();
+        });
+        $('#buttonShow').click(function(event) {
+
+            getPersonsByServer();
+
+        });
+    }
+
+    function modelWindow() {
+
+        $(this).hide();
+        event.preventDefault();
+        $('#overlay').fadeIn(400,
+            function() {
+                $('#modal_formFive')
+                    .css('display', 'block')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
+    }
+
+    function postPersonsToServer() {
+
+        valueName = $("#signUpName").val();
+        valueSurname = $("#signUpSurname").val();
+        valueAge = $("#signUpAge").val();
+        valuePassword = $("#signUpPassword").val();
+        valueConfirmPasssword = $("#signUpConfirmPassword").val();
+        valueRole = $("#selectRole option:selected").text();
+
+        if (valuePassword === valueConfirmPasssword) {
+            $.ajax({
+                url: "http://localhost:8082/user",
+                headers: {
+                    'header-id': headerId,
+                    'header-role': headerRole
+                },
+                type: "POST",
+                data: {
+                    name: valueName,
+                    surname: valueSurname,
+                    age: valueAge,
+                    password: valuePassword,
+                    role: valueRole
+                },
+                success: function() {
+
+                    alert("Add person success");
+                    console.log('Successfully connected to the server');
+
+                },
+                error: function() {
+
+                    alert("Wrong name or surname or age");
+                    console.log('Something went wrong');
+                }
+            });
+            $('#modal_formFive')
+                .animate({
+                        opacity: 0,
+                        top: '45%'
+                    }, 200,
+                    function() {
+                        $(this).css('display', 'none');
+                        $('#overlay').fadeOut(400);
+                    }
+                );
+        } else {
+            $("#areaForError").text("Passwords do not match");
         }
     }
-	
-});
-	}
 
-	function deletePersonByServer(targetId){
-		$.ajax({
-    url: urlPage + targetId,
-    type: 'DELETE',
-    success: function(result) {
+    function getPersonsByServer() {
 
-    	alert(result);
-        console.log('Successfully connected to the server');
-    },
-    error: function(result) {
+        $.ajax({
+            url: "http://localhost:8082/users",
+            headers: {
+                'header-id': headerId,
+                'header-role': headerRole
+            },
+            type: "GET",
+            success: function(data) {
+                //data = Object.values(data);
+                // data = Array.from(data);
+                console.log(data);
 
-    	alert(result);
-        console.log('Error');
+
+                var tableForPersons = document.createElement('div');
+                if (document.getElementById('userstable')) {
+                    var elem = document.getElementById('userstable');
+                    elem.parentNode.removeChild(elem);
+                }
+                tableForPersons.setAttribute('id', 'userstable');
+                var table = document.createElement('table');
+                $.each(data, function(index, value) {
+                    if (value !== null) {
+                        var tr = document.createElement('tr');
+                        tr.innerHTML = '<td>' + value.name + '</td>' +
+                            '<button class="EditByPerson">Edit</button>' +
+                            '<button class="info">Info</button>' +
+                            '<button class="del">Delete</button>';
+                        tr.setAttribute('id', value.id);
+                        table.appendChild(tr);
+                    }
+
+
+                });
+
+                tableForPersons.appendChild(table);
+                document.body.appendChild(tableForPersons);
+
+                tableForPersons.onclick = function(event) {
+
+                    var target = event.target;
+                    var targetId = +target.parentNode.getAttribute('id');
+
+
+                    if (target.className == 'del') {
+
+                        deletePersonByServer(targetId);
+                    }
+                    if (target.className == 'info') {
+                        getInfoPersonbyServer(targetId);
+                    }
+                    if (target.className == 'EditByPerson') {
+                        $('#buttonSignUp').unbind();
+                        postUpdatePersonToServer(targetId);
+                    }
+                }
+
+            },
+            error: function(data) {
+
+                alert("Wrong name or surname or age");
+                console.log('Something went wrong');
+            }
+        });
+
     }
-});
-	}
-	function modelWindow(){
-
-		event.preventDefault(); 
-		$('#overlay').fadeIn(400, 
-		 	function(){ 
-				$('#modal_form') 
-					.css('display', 'block') 
-					.animate({opacity: 1, top: '50%'}, 200); 
-		});
-
-	}
 
 
-	function getInfoPersonbyServer(targetId){
+    function deletePersonByServer(targetId) {
+        $.ajax({
+            url: urlPage + targetId,
+            headers: {
+                'header-id': headerId,
+                'header-role': headerRole
+            },
+            type: 'DELETE',
+            success: function(result) {
 
-		event.preventDefault(); 
-		$('#overlay').fadeIn(400, 
-		 	function(){ 
-				$('#modal_formTwo') 
-					.css('display', 'block') 
-					.animate({opacity: 1, top: '50%'}, 200); 
-		});
+                alert(result);
+                console.log('Successfully connected to the server');
+            },
+            error: function(result) {
 
-		$.get( urlPage +targetId, function( data ) {
-			console.log(data.name);
+                alert(result.responseText);
+                console.log('Error');
+            }
+        });
+    }
 
-			 $( "#infoName" ).text(data.name);
-			 $( "#infoSurname" ).text(data.surname); 
-			 $( "#infoAge" ).text(data.age);  
 
-   });
-	}
+    function getInfoPersonbyServer(targetId) {
 
-	function postUpdatePersonToServer(targetId)
-	{
-		event.preventDefault(); 
-		$('#overlay').fadeIn(400, 
-		 	function(){ 
-				$('#modal_formThree') 
-					.css('display', 'block') 
-					.animate({opacity: 1, top: '50%'}, 200); 
-		});
+        event.preventDefault();
+        $('#overlay').fadeIn(400,
+            function() {
+                $('#modal_formTwo')
+                    .css('display', 'block')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
 
-		$.get( urlPage + targetId, function( data ) {
+        $.get(urlPage + targetId, function(data) {
 
-			 $( "#updateName" ).val(data.name);
-			 $( "#updateSurname" ).val(data.surname); 
-			 $( "#updateAge" ).val(data.age); 
-		});
+            $("#infoName").text(data.name);
+            $("#infoSurname").text(data.surname);
+            $("#infoAge").text(data.age);
+            $("#infoRole").text(data.role);
+        });
+    }
 
-		$("#UPDATE").click(function(event){
+    function postUpdatePersonToServer(targetId) {
 
-		valueName = $("#updateName").val();
-		valueSurname = $("#updateSurname").val();
-		valueAge = $("#updateAge").val();
-		$.ajax({
-			url: urlPage + targetId,
-			type:"POST",
-			data:{name:valueName,surname:valueSurname,age:valueAge },
-			success: function(data){
 
-				//alert(data);
-				console.log('Successfully connected to the server');
+        $.ajax({
+            url: urlPage + targetId,
+            headers: {
+                'header-id': headerId,
+                'header-role': headerRole
+            },
+            type: "GET",
+            success: function(data) {
 
-			},
-			error: function(){
+            	 modelWindow();
+                $("#signUpName").val(data.name);
+                $("#signUpSurname").val(data.surname);
+                $("#signUpAge").val(data.age);
 
-				//alert(" Bad update person");
-				console.log('Something went wrong');
-			}
-		});
+            },
+            error: function(data) {
 
-	});
-	}
+                alert(data.responseText);
+                console.log('Something went wrong');
+            }
+
+        });
+
+        $("#buttonSignUp").click(function(event) {
+
+            valueName = $("#signUpName").val();
+            valueSurname = $("#signUpSurname").val();
+            valueAge = $("#signUpAge").val();
+            $.ajax({
+                url: urlPage + targetId,
+                headers: {
+                    'header-id': headerId,
+                    'header-role': headerRole
+                },
+                type: "POST",
+                data: {
+                    name: valueName,
+                    surname: valueSurname,
+                    age: valueAge
+                },
+                success: function(data) {
+
+                    //alert(data);
+                    console.log('Successfully connected to the server');
+
+                },
+                error: function() {
+
+                    //alert(" Bad update person");
+                    console.log('Something went wrong');
+                }
+            });
+        });
+    }
 });
--- server.js
+++ server.js
@@ -3,111 +3,187 @@ var expressValidator = require('express-validator');
 var bodyParser = require('body-parser');
 var app = express();
 var port = 8082;
+var headerId, headerRole;
+const Role_Admin = "Admin";
+const Role_Guest = "Guest";
+const Role_User = "User";
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(expressValidator());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 
 var persons = [];
-var id = 0 ;
+var id = 0;
 
-function Human(name,surname,age,id){
-	this.name = name;
-	this.surname = surname;
-	this.age = age;
-	this.id = id;
+function Human(name, surname, age, id, password, role) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.password = password;
+    this.role = role;
+    this.id = id;
 }
 var human = new Human();
 
-app.post('/user',function(req,res){
-	//console.log("POST_ONE");
+app.use(function(req, res, next) {
+
+    headerId = req.headers["header-id"];
+    headerRole = req.headers["header-role"];
+    next();
+});
+
+app.use('/user',function(req, res, next) {
+
 	var data = req.body;
-	var newHuman = new Human(data.name, data.surname, data.age, id);
+	 var arrayNames = persons.map( (item) => item.name );
+	 var find = arrayNames.includes(data.name);
+	
+	if(find == false)
+	{
+		next();
 
+	}
+	else{
+		res.status(400).send("Wrong");
+	}
 
-	req.checkBody('name', 'name is required').notEmpty().isAlpha();
-	req.checkBody('surname', 'surname is required').notEmpty().isAlpha();
-	req.checkBody('age', 'age is required').notEmpty().isInt();
-
-
-  var errors = req.validationErrors();
-  if (errors) {
-    res.status(400).send('Error: ' + errors.message);
-    return;
-  }else
-  {
-
-  	persons.push(newHuman);
-	res.send(String(id++));
-}
 });
 
-app.post('/user/:id',function(req,res){
-	console.log("UPDATE");
+app.use('/user/:id',function(req, res, next) {
+
 	var data = req.body;
+	 var arrayNames = persons.map( (item) => item.name );
+	 var find = arrayNames.includes(data.name);
+	
+	if(find == false)
+	{
+		next();
 
-		if(persons[req.params.id])
-		{
-			req.checkBody('name', 'name is required').notEmpty().isAlpha();
-			req.checkBody('surname', 'surname is required').notEmpty().isAlpha();
-			req.checkBody('age', 'age is required').notEmpty().isInt();
-
-
-  			var errors = req.validationErrors();
- 			if (errors) {
-    		res.status(400).send('Error: ' + errors.message);
-    		return;
-  		}
-
-			var oldPersons = persons[req.params.id];
-			oldPersons.name = data.name;
-        	oldPersons.surname = data.surname;
-        	oldPersons.age = data.age;
-			res.send("Sucusess");
-			console.log(persons[req.params.id]);
-		}
-		else
-		{
-			res.send("Fail");
-		}
-
-});
-app.get('/user/:id',function(req,res){
-
-		if(persons[req.params.id])
-		{
-			res.send(persons[req.params.id]);
-		}
-		else
-		{
-			res.send("Fail");
-		}
-
-	console.log("GET_One_users");
-
-});
-app.delete('/user/:id',function(req,res){
-	console.log("DELETE");
-
-		if(persons[req.params.id])
-		{
-			delete persons[req.params.id];
-			res.send("User delete");
-			console.log(persons);
-		}
-		else
-		{
-			res.send("Fail");
-
-		}
+	}
+	else{
+		res.status(400).send("Wrong");
+	}
 
 });
 
-app.get('/users',function(req,res){
-	res.set("Access-Control-Allow-Origin", "*");
-	res.send(persons);
-	console.log("GET_USERS");
+
+
+app.post('/user', function(req, res) {
+    var data = req.body;
+    var newHuman = new Human(data.name, data.surname, data.age, id, data.password, data.role);
+
+
+    req.checkBody('name', 'name is required').notEmpty().isAlpha();
+    req.checkBody('surname', 'surname is required').notEmpty().isAlpha();
+    req.checkBody('age', 'age is required').notEmpty().isInt();
+
+
+    var errors = req.validationErrors();
+    if (errors) {
+        res.status(400).send('Error: ' + errors.message);
+        return;
+    } else {
+
+        persons.push(newHuman);
+        res.status(200).send(String(id++));
+    }
+});
+
+app.post('/user/:id', function(req, res) {
+    var data = req.body;
+
+
+
+    if (persons[req.params.id]) {
+
+        req.checkBody('name', 'name is required').notEmpty().isAlpha();
+        req.checkBody('surname', 'surname is required').notEmpty().isAlpha();
+        req.checkBody('age', 'age is required').notEmpty().isInt();
+
+
+        var errors = req.validationErrors();
+        if (errors) {
+            res.status(400).send('Error: ' + errors.message);
+            return;
+        }
+
+        var oldPersons = persons[req.params.id];
+        oldPersons.name = data.name;
+        oldPersons.surname = data.surname;
+        oldPersons.age = data.age;
+        res.status(200).send("Sucusess");
+
+    } else {
+        res.status(400).send("Fail");
+    }
+
+
+});
+app.post('/loginuser', function(req, res) {
+
+    var data = req.body;
+    for (var i = 0; i < persons.length; i++) {
+        if (data.name === persons[i].name && data.password == persons[i].password) {
+            res.status(200);
+            res.json({
+                'role': persons[i].role,
+                'id': persons[i].id
+            });
+            return;
+        }
+    }
+    res.status(400).send("Wrong");
+
+});
+
+app.get('/user/:id', function(req, res) {
+
+    if (headerRole == Role_Guest) {
+
+        res.status(403).send("Forbidden");
+    }
+
+    if (persons[req.params.id]) {
+        res.status(200).send(persons[req.params.id]);
+    } else {
+        res.status(400).send("Fail");
+    }
+
+
+});
+app.delete('/user/:id', function(req, res) {
+
+    if (headerRole == Role_Admin) {
+        if (persons[req.params.id]) {
+            delete persons[req.params.id];
+            res.status(200).send("User delete");
+        } else {
+            res.status(400).send("Fail");
+
+        }
+    } else {
+        res.status(403).send("Forbidden");
+    }
+
+});
+
+app.get('/users', function(req, res) {
+
+
+
+    if (headerRole == Role_User || headerRole == Role_Guest) {
+
+        res.status(200).send(new Human(persons[headerId]));
+
+    } else {
+
+        res.status(200).send(persons);
+
+
+    }
 
 });
 
